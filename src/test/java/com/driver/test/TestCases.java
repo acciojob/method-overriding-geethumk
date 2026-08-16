243e83a0df257a6078849e6eba4a296353c83e96
M K Geethika <geethigeethika16@gmail.com>
2026-08-16 20:30:26 +0530

    method-overring

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -2,7 +2,17 @@ package com.driver.test;
 
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
+import com.driver.B;
 
 public class TestCases {
-    
+
+    @Test
+    public void testMethodOverriding() {
+        B obj = new B();
+
+        assertEquals(
+                "Method is overridden in Extended class B",
+                obj.meth()
+        );
+    }
 }
