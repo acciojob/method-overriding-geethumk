18ef5bf40255076a33c1b4d4530e999881a8f792
M K Geethika <geethigeethika16@gmail.com>
2026-08-16 20:05:14 +0530

    method-overring

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class B extends A{
     @Override
-    String meth() {
+    public String meth() {
         return "Method is overridden in Extendend class B";
     }
 }
