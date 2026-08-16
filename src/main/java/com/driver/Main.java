05b9fb4fa0bae65e18ed25390dfe60d6e9938ee3
M K Geethika <geethigeethika16@gmail.com>
2026-08-16 15:27:19 +0530

    method-overring

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,8 +2,8 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        B b = new B();
-        System.out.print(b.meth());
+        B obj = new B();
+        System.out.print(obj.meth());
     }
 
 }
